4a0a147884244fd696d5e4b1019ea01e21afb340
kimdlzp <kimdlzp@gmail.com>
2021-12-24 18:04:41 +0900

    Loading Username

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-const loginFoirm = document.querySelector("#login-form");
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
+  localStorage.setItem("USERNAME_KEY", username);
+  paintGreeting(username);
+}
+
+function paintGreeting(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const saveUsername = localStorage.getItem(USERNAME_KEY);
+
+if (saveUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreeting(saveUsername);
+}
